53046f48f70d3ba31bce5105efb543f884e3bb7d
unknown <you@example.com>
2025-03-25 09:26:12 -0500

    Refinar

--- Proyecto_Inventario/Proyecto/settings.py
+++ Proyecto_Inventario/Proyecto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wl__v#0tk8+r6ow4u*3yyr$pg2lhvooc4*bcj9aqd)(ik0$zxt'
+SECRET_KEY = 'django-insecure-0p5^d@9o+cj((bk3xtpbvcdt^9h+7==2zm#*&bvd39-l3u_=^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
