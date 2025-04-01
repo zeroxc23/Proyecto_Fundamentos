a57520f7d92f8712e739c2cea58a23b81997972a
unknown <you@example.com>
2025-04-01 09:58:57 -0500

    Aplicacion

---
+++ Proyecto_Inventario/Aplicaciones/Inventario/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class InventarioConfig(AppConfig):
+    name = 'Aplicaciones.inventario'  
--- Proyecto_Inventario/Proyecto/settings.py
+++ Proyecto_Inventario/Proyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicaciones.Inventario',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'inventario',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   
+        'PORT': '3306',
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'  
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
